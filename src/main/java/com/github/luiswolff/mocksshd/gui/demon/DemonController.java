b8f918070644c427dffcf8beee7a91dad3bac5a5
Luis Wolff <luis-manuel.wolff@outlook.com>
2021-10-25 12:24:21 +0200

    enhanced UI
    The UI dose now have more input componentes and the layout were restructured

--- src/main/java/com/github/luiswolff/mocksshd/gui/demon/DemonController.java
+++ src/main/java/com/github/luiswolff/mocksshd/gui/demon/DemonController.java
@@ -1,6 +1,8 @@
 package com.github.luiswolff.mocksshd.gui.demon;
 
 import java.awt.Container;
+import java.awt.Desktop;
+import java.io.File;
 import java.io.IOException;
 import java.io.Serializable;
 
@@ -9,39 +11,47 @@ import com.github.luiswolff.mocksshd.sshd.StartCommand;
 
 public class DemonController implements Serializable {
 
-    private static final long  serialVersionUID = -1987924477363435321L;
+  private static final long serialVersionUID = -1987924477363435321L;
 
-    private StartCommand       model            = new StartCommand();
-    private DemonPanel         view             = new DemonPanel(this, model);
+  private final StartCommand model = new StartCommand();
+  private final DemonPanel view = new DemonPanel(this, model);
 
-    void startSshd() {
-        MockSshDemon mockSshDemon = MockSshDemon.getInstance();
-        try {
-            mockSshDemon.start(model);
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        if (mockSshDemon.isRunning()) {
-            view.demonStarted();
-        }
+  void startSshd() {
+    MockSshDemon mockSshDemon = MockSshDemon.getInstance();
+    try {
+      mockSshDemon.start(model);
+    } catch (IOException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
     }
-
-    void stopSshd() {
-        MockSshDemon mockSshDemon = MockSshDemon.getInstance();
-        try {
-            mockSshDemon.stop();
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        if (!mockSshDemon.isRunning()) {
-            view.demonStopped();
-        }
+    if (mockSshDemon.isRunning()) {
+      view.demonStarted();
     }
+  }
 
-    public Container getView() {
-        return view;
+  void stopSshd() {
+    MockSshDemon mockSshDemon = MockSshDemon.getInstance();
+    try {
+      mockSshDemon.stop();
+    } catch (IOException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
     }
+    if (!mockSshDemon.isRunning()) {
+      view.demonStopped();
+    }
+  }
 
+  void openRoot() {
+    try {
+      Desktop.getDesktop().open(new File(model.getRoot()));
+    } catch (IOException e) {
+      // TODO
+      e.printStackTrace();
+    }
+  }
+
+  public Container getView() {
+    return view;
+  }
 }
--- src/main/java/com/github/luiswolff/mocksshd/gui/demon/DemonPanel.java
+++ src/main/java/com/github/luiswolff/mocksshd/gui/demon/DemonPanel.java
@@ -1,74 +1,112 @@
 package com.github.luiswolff.mocksshd.gui.demon;
 
+import com.github.luiswolff.mocksshd.sshd.StartCommand;
+import java.awt.BorderLayout;
 import java.awt.FlowLayout;
+import java.awt.GridLayout;
 import java.awt.event.ActionListener;
-
+import java.util.Arrays;
 import javax.swing.JButton;
+import javax.swing.JComponent;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-import com.github.luiswolff.mocksshd.sshd.StartCommand;
-
 public class DemonPanel extends JPanel {
 
-    private static final long     serialVersionUID = -6403969028249994552L;
+  private static final long serialVersionUID = -6403969028249994552L;
 
-    private final DemonController controller;
-    private final StartCommand    model;
+  private final DemonController controller;
+  private final StartCommand model;
 
-    private final JTextField      portInput;
-    private final JButton         startStopButto;
+  private final JTextField portInput;
+  private final JTextField hostInput;
+  private final JTextField rootInput;
+  private final JTextField userInput;
+  private final JTextField passInput;
+  private final JButton startStopButton;
 
-    private boolean               demonRunning;
+  private boolean demonRunning;
 
-    public DemonPanel(DemonController controller, StartCommand model) {
-        super(new FlowLayout());
+  public DemonPanel(DemonController controller, StartCommand model) {
+    super(new FlowLayout());
 
-        this.controller = controller;
-        this.model = model;
+    this.controller = controller;
+    this.model = model;
 
-        portInput = addInput("Port", this.model.getPort());
-        startStopButto = addButton("start", l -> startStop());
+    JPanel mainPanel = new JPanel();
+    mainPanel.setLayout(new BorderLayout());
+
+    JPanel inputPanel = new JPanel(new GridLayout(5, 2, 10, 10));
+    portInput = addInput(inputPanel, "Port", model.getPort());
+    hostInput = addInput(inputPanel, "Host", model.getHost());
+    rootInput = addInput(inputPanel, "Root", model.getRoot());
+    userInput = addInput(inputPanel, "User", model.getUser());
+    passInput = addInput(inputPanel, "Password", model.getPass());
+    mainPanel.add(inputPanel, BorderLayout.CENTER);
+
+    JPanel buttonPanel = new JPanel();
+    addJButton(buttonPanel, "Open Root", l -> controller.openRoot());
+    startStopButton = addJButton(buttonPanel, "start", l -> startStop());
+    mainPanel.add(buttonPanel, BorderLayout.SOUTH);
+
+    add(mainPanel);
+  }
+
+  private JButton addJButton(JPanel buttonPanel, String title, ActionListener actionListener) {
+    JButton jButton = new JButton(title);
+    jButton.addActionListener(actionListener);
+    buttonPanel.add(jButton);
+    return jButton;
+  }
+
+  private static JTextField addInput(JComponent parent, String labelText, Object textValue) {
+    JLabel label = new JLabel(labelText);
+    JTextField input = new JTextField();
+    input.setText(textValue.toString());
+    label.setLabelFor(input);
+    parent.add(label);
+    parent.add(input);
+    return input;
+  }
+
+  private void startStop() {
+    if (demonRunning) {
+      controller.stopSshd();
+    } else {
+      writeModel();
+      controller.startSshd();
     }
+  }
 
-    private JTextField addInput(String labelText, int port) {
-        JLabel label = new JLabel(labelText);
-        JTextField input = new JTextField();
-        input.setText(Integer.toString(port));
-        label.setLabelFor(input);
-        add(label);
-        add(input);
-        return input;
-    }
+  private void writeModel() {
+    model.setPort(Integer.parseInt(portInput.getText()));
+    model.setHost(hostInput.getText());
+    model.setRoot(rootInput.getText());
+    model.setUser(userInput.getText());
+    model.setRoot(passInput.getText());
+  }
 
-    private JButton addButton(String title, ActionListener action) {
-        JButton button = new JButton(title);
-        button.addActionListener(action);
-        add(button);
-        return button;
-    }
+  void demonStarted() {
+    inputsSetEnabled(false);
+    startStopButton.setText("stop");
+    demonRunning = true;
+    updateUI();
+  }
 
-    private void startStop() {
-        if (demonRunning) {
-            controller.stopSshd();
-        } else {
-            controller.startSshd();
-        }
-    }
+  void demonStopped() {
+    inputsSetEnabled(true);
+    startStopButton.setText("start");
+    demonRunning = false;
+    updateUI();
+  }
 
-    void demonStarted() {
-        portInput.setEnabled(false);
-        startStopButto.setText("stop");
-        demonRunning = true;
-        updateUI();
-    }
+  private void inputsSetEnabled(boolean enabled) {
+    allInputs().forEach(input -> input.setEnabled(enabled));
+  }
 
-    void demonStopped() {
-        portInput.setEnabled(true);
-        startStopButto.setText("start");
-        demonRunning = false;
-        updateUI();
-    }
+  private Iterable<JTextField> allInputs() {
+    return Arrays.asList(portInput, hostInput, rootInput, userInput, passInput);
+  }
 
 }
